83d967c02d7493c3dcf2225514982bd2d1f223b2
mhanuz <46758023+mhanuz@users.noreply.github.com>
2021-03-12 23:26:37 +0600

    Delete Basic directory

--- Basic/condition.py
+++
@@ -1,8 +0,0 @@
-num=int(input('ENter a number'))
-
-if num>0:
-    print('positive')
-elif n<0:
-    print('Negative')
-else:
-    print('Zero')
--- Basic/mspack/msstring.py
+++
@@ -1,9 +0,0 @@
-
-def fullName(fist_name,last_name):
-    return fist_name+last_name
-
-
-
-
-def nameRevers(fist_name,last_name):
-    return last_name+fist_name
